78336ea2fb6531a3688425c79903e4c71bfce949
fahim59 <mrahaman59@yahoo.com>
2023-08-11 19:35:43 +0600

    baseclass added

---
+++ src/main/java/BaseClass.java
@@ -0,0 +1,22 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BaseClass {
+    public static WebDriver driver;
+
+    public static void FirefoxLaunch() {
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+    }
+
+    public static void OpenWebsite(String Url) {
+        driver.get(Url);
+    }
+
+    public static void BrowserQuit() {
+        driver.quit();
+    }
+
+    public static void SmallWait(int second) throws InterruptedException {Thread.sleep(second);}
+}
